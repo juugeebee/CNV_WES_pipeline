f91c5476128b396bdb1e04d057150b0f062b82c6
juugeebee <july2023@yahoo.fr>
2020-07-29 15:29:14 +0200

    no time

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -21,12 +21,12 @@ FLOWCELL="$(zcat $R1 | head -1 | awk '{print $1}' | cut -d ":" -f 3)"; \
 DEVICE="$(zcat $R1 | head -1 | awk '{print $1}' | cut -d ":" -f 1 | cut -d "@" -f 2)"; \
 BARCODE="$(zcat $R1 | head -1 | awk '{print $2}' | cut -d ":" -f 4)"; \
 RG=$(echo "\"@RG\tID:${DEVICE}.${FLOWCELL}.${SAMPLE}\tPU:${FLOWCELL}\tSM:${SAMPLE}\tPL:ILLUMINA\tLB:${SAMPLE}-${BARCODE}\"") \
-MAPPING_CMD=$(echo "time bwa mem -M -t 18 -R $RG $REF $R1 $R2 | samtools sort -@ 4 -o ${SAMPLE}.bam -"); \
+MAPPING_CMD=$(echo "bwa mem -M -t 18 -R $RG $REF $R1 $R2 | samtools sort -@ 4 -o ${SAMPLE}.bam -"); \
 eval $MAPPING_CMD;\
 done;
 
 ### MARK DUPLICATES
-time for i in *.bam; do SAMPLE=${i%.*}; time sambamba markdup -t 12 ${SAMPLE}.bam ${SAMPLE}.dedup.bam; done
+for i in *.bam; do SAMPLE=${i%.*}; time sambamba markdup -t 12 ${SAMPLE}.bam ${SAMPLE}.dedup.bam; done
 
 echo ""
 echo "fastq_to_bam.sh job done!"
