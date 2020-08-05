60caee52e981eeb19c1ab0fbaaf8c2caa73f96cb
juugeebee <july2023@yahoo.fr>
2020-08-05 15:16:16 +0200

    bowtie 2

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -8,25 +8,42 @@ echo "fastq_to_bam.sh start"
 echo ""
 
 REF="/media/Data1/jbogoin/ref/hg38_Mlast/hg38_GenDev.fa"
+DATA=$PWD
 
 ### REMOVE previous dedup.bam files ###
 rm -f *.bam*
-rm -Rf multiqc*
 
 ## MAPPING BWA SEQUENTIAL 
-for R1 in *_R1_*.fastq.gz; \
-do R2=${R1/_R1_/_R2_}; \
-SAMPLE=${R1%%_*}; \
-FLOWCELL="$(zcat $R1 | head -1 | awk '{print $1}' | cut -d ":" -f 3)"; \
-DEVICE="$(zcat $R1 | head -1 | awk '{print $1}' | cut -d ":" -f 1 | cut -d "@" -f 2)"; \
-BARCODE="$(zcat $R1 | head -1 | awk '{print $2}' | cut -d ":" -f 4)"; \
-RG=$(echo "\"@RG\tID:${DEVICE}.${FLOWCELL}.${SAMPLE}\tPU:${FLOWCELL}\tSM:${SAMPLE}\tPL:ILLUMINA\tLB:${SAMPLE}-${BARCODE}\"") \
-MAPPING_CMD=$(echo "bwa mem -M -t 18 -R $RG $REF $R1 $R2 | samtools sort -@ 4 -o ${SAMPLE}.bam -"); \
-eval $MAPPING_CMD;\
-done;
+for R1 in *_R1_*.fastq.gz; 
+    
+    do 
+    R2=${R1/_R1_/_R2_}; 
+
+    SAMPLE=${R1%%_*};
+
+    cd /media/Data2/jbogoin/ref;
+
+    bowtie2 -p 36 -x hg38_GenDev -1 $DATA/$R1 -2 $DATA/$R2 -S $DATA/$SAMPLE.sam;
+
+    cd $DATA
+
+    samtools view -@ 36 -bS $SAMPLE.sam -o $SAMPLE.bam;
+
+    samtools sort -@ 36 $SAMPLE.bam -o $SAMPLE.sorted.bam;
+
+    #samtools index -@ 12 $SAMPLE.sorted.bam;
+
+done
 
 ### MARK DUPLICATES
-for i in *.bam; do SAMPLE=${i%.*}; time sambamba markdup -t 12 ${SAMPLE}.bam ${SAMPLE}.dedup.bam; done
+for i in *sorted.bam; 
+    
+    do 
+    SAMPLE=${i%.*};
+ 
+    sambamba markdup -t 36 ${SAMPLE}.bam ${SAMPLE}.dedup.bam;
+    
+done
 
 echo ""
 echo "fastq_to_bam.sh job done!"
